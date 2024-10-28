980c6ae957771db21d35d1da382883f424b4667e
zawakichi <zawakichi21@gmail.com>
2024-10-28 23:52:21 +0900

    denodbのエラー対応

--- vite-project/src/infra/dbaccesser.ts
+++ vite-project/src/infra/dbaccesser.ts
@@ -1,13 +1,38 @@
-import { Database, MySQLConnector } from 'https://deno.land/x/denodb/mod.ts';
-
+import { Database, Model, DataTypes ,MySQLConnector } from 'https://deno.land/x/denodb/mod.ts';
 const connector = new MySQLConnector({
-  database: 'my-database',
-  host: 'url-to-db.com',
-  username: 'username',
-  password: 'password',
+  database: 'hogehoge',
+  host: 'host.docker.internal',
+  username: 'root',
+  password: 'hogehoge',
   port: 3306, // optional
 });
 
 const db = new Database(connector);
+class Flight extends Model {
+  static table = 'flights';
+  static timestamps = true;
 
-export default db;
+  static fields = {
+    id: { primaryKey: true, autoIncrement: true },
+    departure: DataTypes.STRING,
+    destination: DataTypes.STRING,
+    flightDuration: DataTypes.FLOAT,
+  };
+
+  static defaults = {
+    flightDuration: 2.5,
+  };
+}
+
+db.link([Flight]);
+
+await db.sync({ drop: true });
+
+try {
+  await Flight.create({
+    departure: 'Paris',
+    destination: 'Tokyo',
+  });
+} catch (e: unknown) {
+  console.log(e)
+}
